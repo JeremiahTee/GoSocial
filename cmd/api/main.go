1d3ded0833809c160649ef65ebb72593d2cd8135
JeremiahTee <jeremiahteee@gmail.com>
2025-03-09 18:32:17 -0400

    hot reloading with air

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "log"
+import (
+	"github.com/JeremiahTee/GoSocial/internal/env"
+	"log"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
