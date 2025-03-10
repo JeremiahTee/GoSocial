af2c4522bbfc96c789eb6ab9b2518ea5c6dc50a1
JeremiahTee <jeremiahteee@gmail.com>
2025-03-09 21:03:04 -0400

    json utils

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -16,6 +16,15 @@ type application struct {
 
 type config struct {
 	addr string
+	db   dbConfig
+	env  string
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() http.Handler {
