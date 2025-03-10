398f36d2bf020f9407cbe4f508ec4d7d0df06c6e
JeremiahTee <jeremiahteee@gmail.com>
2025-03-09 21:02:47 -0400

    create_users, create_posts migration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"github.com/JeremiahTee/GoSocial/internal/db"
 	"github.com/JeremiahTee/GoSocial/internal/env"
 	"github.com/JeremiahTee/GoSocial/internal/store"
 	"log"
 )
 
+const version = "0.0.1"
+
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
+		env: env.GetString("ENV", "development"),
 	}
+
+	database, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer database.Close()
+	log.Println("database connection pool established")
+
+	storage := store.NewStorage(database)
 
 	app := &application{
 		config: cfg,
-		store:  store.NewStorage(nil),
+		store:  storage,
 	}
 
 	mux := app.mount()
+
 	log.Fatal(app.run(mux))
 }
