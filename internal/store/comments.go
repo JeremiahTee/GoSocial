7af46b10ee268be62ebb79574f7517834e778035
JeremiahTee <jeremiahteee@gmail.com>
2025-03-10 23:55:45 -0400

    fix query & migration

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -20,9 +20,9 @@ type CommentsStore struct {
 
 func (s *CommentsStore) GetByPostID(ctx context.Context, postID int64) ([]Comment, error) {
 	query := `
-	SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, users.username, user.user.id FROM comments c
-	JOIN users on users.id = c.user_.id
-	WHERE c.post_id = 2;
+	SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, users.username, users.id FROM comments c
+	JOIN users on users.id = c.user_id
+	WHERE c.post_id = $1
 	ORDER BY c.created_at DESC;
 	`
 
